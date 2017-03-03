a1d5495dfa75a205fb090ea0a75789fa2f5ce0e2
Francois-L <francois.lair8@gmail.com>
2017-03-03 15:56:59 +0100

    Update database.php

--- database.php
+++ database.php
@@ -9,19 +9,34 @@ catch(Exception $e)
 }		
 
 ?>
+
+<?php
+session_start();
+
+$request = $bdd->query('SELECT objectif, date FROM illdo');
+
+$donnees = $request->fetch();
+
+echo ' Objectif : ' . $donnees['objectif'] . ' - Date de fin : ' . $donnees['date'];
+$reponse->closeCursor();
+
+?>
+
+
+
 <?php 
 session_start();
 
 
-
 $send1 = !empty($_POST['send1']) ? $_POST['send1'] : NULL;
 $send2 = !empty($_POST['send2']) ? $_POST['send2'] : NULL;
- 
-//Ton traitement de tri
+
+ //Ton traitement de tri
 
 $req = $bdd->prepare("INSERT INTO illdo(objectif, date) VALUES('$send1', '$send2')");
-$req ->execute(array($_POST['send1']));
-$req ->execute(array($_POST['send2']));
+$req ->execute(array($_POST['send1'], $_POST['send2']));
+
+var_dump($_POST);
 ?>
 
  <?php                                                     

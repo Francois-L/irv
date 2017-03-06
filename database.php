9a171276af38734f11ab8908e817e1bd2ada891e
Francois-L <francois.lair8@gmail.com>
2017-03-06 13:48:27 +0100

    Update database.php

--- database.php
+++ database.php
@@ -29,9 +29,9 @@ $send1 = !empty($_POST['send1']) ? $_POST['send1'] : NULL;
 $send2 = !empty($_POST['send2']) ? $_POST['send2'] : NULL;
 
  //Ton traitement de tri
-
-$req = $bdd->prepare("INSERT INTO illdo(objectif, date) VALUES('$send1', '$send2')");
-$req ->execute(array($_POST['send1'], $_POST['send2']));
-
+if(!empty($_POST['send1']) and !empty($_POST['send2'])) {
+	$req = $bdd->prepare("INSERT INTO illdo(objectif, date) VALUES('$send1', '$send2')");
+	$req ->execute(array ($_POST['send1'], $_POST['send2']));
+}
 
 exit(0);

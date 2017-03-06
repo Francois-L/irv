0d49ae3a412802c1066788eec9f8b330c8483600
Francois-L <francois.lair8@gmail.com>
2017-03-06 14:29:24 +0100

    Update load-objectifs.php

--- load-objectifs.php
+++ load-objectifs.php
@@ -14,6 +14,6 @@ $request = $bdd->query('SELECT objectif, date, ID FROM illdo');
 
 while ($donnees = $request->fetch()) 
 {
-	echo '<p id="dynamicValue1-' . $donnees['ID'] . '">' . $donnees['objectif'] . '</p><p id="dynamicValue2-' . $donnees['ID'] . '">' . $donnees['date'] . '</p><li id="over-'. $donnees['ID'] .'">Terminé</li><li id="delete-'. $donnees['ID'] .'">Supprimer</li><img id="working'. $donnees['ID'] .'" src="logo.png" />';
+	echo '<div id="box_' . $donnees['ID'] . '" class="col-xs-6 col-sm-4 col-md-3 col-lg-3"><p id="dynamicValue1-' . $donnees['ID'] . '">' . $donnees['objectif'] . '</p><p id="dynamicValue2-' . $donnees['ID'] . '">' . $donnees['date'] . '</p><li id="over-'. $donnees['ID'] .'">Terminé</li><li id="delete-'. $donnees['ID'] .'">Supprimer</li><img id="working'. $donnees['ID'] .'" src="logo.png" /></div>';
 }
 

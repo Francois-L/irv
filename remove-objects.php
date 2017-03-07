7293aa7d3e5f41dd779f9809e5ec7ab4c1f507ea
Francois-L <francois.lair8@gmail.com>
2017-03-07 16:21:03 +0100

    Update remove-objects.php

--- remove-objects.php
+++ remove-objects.php
@@ -8,22 +8,15 @@ catch(Exception $e)
 	die('Erreur : ' . $e->getMessage());
 }		
 
-
-$send1 = !empty($_POST['send1']) ? $_POST['send1'] : NULL;
-$send2 = !empty($_POST['send2']) ? $_POST['send2'] : NULL;
-
- //Ton traitement de tri
-if(!empty($_POST['send1']) and !empty($_POST['send2'])) {
-	$req = $bdd->prepare("INSERT INTO illdo(objectif, date) VALUES('$send1', '$send2')");
-	$req ->execute(array ($_POST['send1'], $_POST['send2']));
-}
-
-
  // Suppression
-$ID = $_GET['ID'];
-$ID = !empty($_POST['ID']) ? $_POST['ID'] : NULL;
-$request = $bdd->query("DELETE FROM illdo WHERE ID = ".$ID );
 
+if (!empty($id)) { 
+  $req=$bdd->prepare("DELETE FROM `illdo` WHERE id=:idObjectif");
+  $req->bindParam(":idObjectif",$id);
+  $req->execute();
+} else {
+  die("il n'y a pas d'id en entrée :/");
+}
 
 
 exit(0);

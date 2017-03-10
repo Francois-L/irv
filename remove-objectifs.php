aa3d2891c204fe5b6c3e99482bcfeaa6dcd3a342
Francois-L <francois.lair8@gmail.com>
2017-03-10 12:19:10 +0100

    Update remove-objectifs.php

--- remove-objectifs.php
+++ remove-objectifs.php
@@ -10,11 +10,18 @@ catch(Exception $e)
 
  // Suppression
 
+
+$id = !empty($_POST['id']) ? $_POST['id'] : NULL;
+
+var_dump($id);
+
 if (!empty($id)) { 
-  $req=$bdd->prepare("DELETE FROM `illdo` WHERE id=:idObjectif");
-  $req->bindParam(":idObjectif",$id);
-  $req->execute();
-} else {
+	$req=$bdd->prepare('DELETE FROM illdo WHERE ID = :idObjectif');
+	$req->bindParam(':idObjectif', id);
+	$req->execute();
+	echo('supprimé');
+} 
+	else {
   die("il n'y a pas d'id en entrée :/");
 }
 

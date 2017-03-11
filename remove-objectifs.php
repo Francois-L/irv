fd3443ca723c2857364497a6100b3524e1427fb7
Francois-L <francois.lair8@gmail.com>
2017-03-11 22:28:08 +0100

    Update remove-objectifs.php

--- remove-objectifs.php
+++ remove-objectifs.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=illdobdd', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=illdobdd', 'root', 'root');
 }
 catch(Exception $e)
 {
@@ -17,7 +17,7 @@ var_dump($id);
 
 if (!empty($id)) { 
 	$req=$bdd->prepare('DELETE FROM illdo WHERE ID = :idObjectif');
-	$req->bindParam(':idObjectif', id);
+	$req->bindParam(':idObjectif', $id);
 	$req->execute();
 	echo('supprimé');
 } 

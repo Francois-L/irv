533355c5d398e46272a7e37bb98e514af96a9f20
Francois-L <francois.lair8@gmail.com>
2017-03-13 14:53:52 +0100

    Update load-objectifs.php

--- load-objectifs.php
+++ load-objectifs.php
@@ -1,18 +1,16 @@
 <?php
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=illdobdd', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=illdobdd', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 }
 catch(Exception $e)
 {
 	die('Erreur : ' . $e->getMessage());
 }		
 
-session_start();
-
 $request = $bdd->query('SELECT objectif, date, ID FROM illdo ORDER BY ID DESC');
 
 while ($donnees = $request->fetch()) 
 {
-	echo '<div id="box_' . $donnees['ID'] . '" class="col-xs-6 col-sm-4 col-md-3 col-lg-3"><p id="dynamicValue1-' . $donnees['ID'] . '">' . $donnees['objectif'] . '</p><p id="dynamicValue2-' . $donnees['ID'] . '">' . $donnees['date'] . '</p><li id="over-'. $donnees['ID'] .'">Terminé</li><li id="delete-'. $donnees['ID'] .'" data-id-objectif="'. $donnees['ID'] .'">Supprimer</li><img id="working'. $donnees['ID'] .'" src="logo.png" /></div>';
+	echo '<div id="box_' . $donnees['ID'] . '" class="col-xs-6 col-sm-4 col-md-3 col-lg-3"><p id="objectif-date-' . $donnees['ID'] .'">' . $donnees['objectif'] . '<br />' . $donnees['date'] . '</p><li id="over-'. $donnees['ID'] .'" data-id-over="'. $donnees['ID'] .'">Terminé</li><li id="delete-'. $donnees['ID'] .'" data-id-objectif="'. $donnees['ID'] .'">Supprimer</li><img id="working'. $donnees['ID'] .'" src="logo.png" /></div>';
 }

3bacd135d70beba5df35f746ba82283ea8a32a26
Francois-L <francois.lair8@gmail.com>
2017-03-14 09:27:31 +0100

    Update load-objectifs.php

--- load-objectifs.php
+++ load-objectifs.php
@@ -12,5 +12,6 @@ $request = $bdd->query('SELECT objectif, date, ID FROM illdo ORDER BY ID DESC');
 
 while ($donnees = $request->fetch()) 
 {
-	echo '<div id="box_' . $donnees['ID'] . '" class="col-xs-6 col-sm-4 col-md-3 col-lg-3"><p id="objectif-date-' . $donnees['ID'] .'">' . $donnees['objectif'] . '<br />' . $donnees['date'] . '</p><li id="over-'. $donnees['ID'] .'" data-id-over="'. $donnees['ID'] .'">Terminé</li><li id="delete-'. $donnees['ID'] .'" data-id-objectif="'. $donnees['ID'] .'">Supprimer</li><img id="working'. $donnees['ID'] .'" src="logo.png" /></div>';
+	echo '<div id="box_' . $donnees['ID'] . '" class="col-xs-6 col-sm-4 col-md-3 col-lg-3"><p id="objectif-date-' . $donnees['ID'] .'" onclick="toggleMenu()">' . $donnees['objectif'] . '<br />' . $donnees['date'] . '</p><ul id="menubox-'. $donnees['ID'] .'" data-id-menubox="'. $donnees['ID'] .'" style="display: block;"><li id="over-'. $donnees['ID'] .'" data-id-over="'. $donnees['ID'] .'">Terminé</li><li id="delete-'. $donnees['ID'] .'" data-id-objectif="'. $donnees['ID'] .'">Supprimer</li></ul><img id="working'. $donnees['ID'] .'" src="logo.png" /></div>';
 }
+

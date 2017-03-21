9238c2cca0b828a9b302c77d0fa9f86868c98822
Francois-L <francois.lair8@gmail.com>
2017-03-21 14:19:54 +0100

    Update load-objectifs.php

--- load-objectifs.php
+++ load-objectifs.php
@@ -2,24 +2,25 @@
 try
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=illdobdd', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+	$bdd = new PDO('mysql:host=localhost;dbname=illdobdd', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 catch(Exception $e)
 {
 	die('Erreur : ' . $e->getMessage());
 }		
 
-$request = $bdd->query('SELECT objectif, DATE_FORMAT(date, "%d-%m-%Y") AS datetri, ID FROM illdo ORDER BY date DESC');
+$request = $bdd->query('SELECT objectif, DATE_FORMAT(date, "%d/%m/%Y") AS datetri, ID FROM illdo ORDER BY date DESC');
 
 while ($donnees = $request->fetch()) 
 {
 	echo '<div id="box_' . $donnees['ID'] . '" class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
 	<p id="objectifdate-' . $donnees['ID'] .'" data-id-objectifdate="'. $donnees['ID'] .'">' . $donnees['objectif'] . '<br />' . $donnees['datetri'] . '</p>
-	<ul id="menubox-'. $donnees['ID'] .'" data-id-menubox="'. $donnees['ID'] .'" style="display: block;">
-	<li id="over-'. $donnees['ID'] .'" data-id-over="'. $donnees['ID'] .'">Terminé</li>
-	<form id="uploadimage-'. $donnees['ID'] .'" action="" method="post" enctype="multipart/form-data">
-	<input type="file" id="file-'. $donnees['ID'] .'">
-	<input type="submit" id="submit-'. $donnees['ID'] .'" value="Envoyer" class="submit" /></form>
-	<li id="delete-'. $donnees['ID'] .'" data-id-objectif="'. $donnees['ID'] .'">Supprimer</li></ul>
+		<ul id="menubox-'. $donnees['ID'] .'" data-id-menubox="'. $donnees['ID'] .'" style="display:none">
+			<li id="over-'. $donnees['ID'] .'" data-id-over="'. $donnees['ID'] .'">Terminé</li>
+				<form id="uploadimage-'. $donnees['ID'] .'" method="post" action="save-pictures.php">
+				<input type="file" id="file-'. $donnees['ID'] .'">
+				<input type="submit" id="submit-'. $donnees['ID'] .'" value="Envoyer" class="submit" /></form>
+			<li id="delete-'. $donnees['ID'] .'" data-id-delete="'. $donnees['ID'] .'">Supprimer</li>
+		</ul>
 	<img id="working'. $donnees['ID'] .'" src="logo.png" /></div>';
 }
-

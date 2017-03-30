1393ba0021059b94f7939a8f84b865a49fedeb33
Francois-L <francois.lair8@gmail.com>
2017-03-30 09:43:39 +0200

    Update load-objectifs.php

--- load-objectifs.php
+++ load-objectifs.php
@@ -14,19 +14,32 @@ while ($donnees = $request->fetch())
 {
 	$image = $donnees['image'];
 	if(empty($image)) {
-		$image = 'default.png';
+		echo '<div id="box_' . $donnees['ID'] . '" class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
+		<p id="objectifdate-' . $donnees['ID'] .'" data-id-objectifdate="'. $donnees['ID'] .'" style="cursor:pointer">' . $donnees['objectif'] . '<br />' . $donnees['datetri'] . '</p>
+			<ul id="menubox-'. $donnees['ID'] .'" data-id-menubox="'. $donnees['ID'] .'" style="display:none" class="menu-objectif">
+				<li id="over-'. $donnees['ID'] .'" data-id-over="'. $donnees['ID'] .'">Partager</li>
+					<form id="uploadimage-'. $donnees['ID'] .'" class="dropzone" method="post" enctype="multipart/form-data">
+					  <input type="hidden" name="target_id" value="'.$donnees['ID'].'"/>
+					  <input type="file" name="target_picture">
+					  <input type="submit" id="submit-'. $donnees['ID'] .'" value="Envoyer" class="submit" />
+					</form>
+				<li id="delete-'. $donnees['ID'] .'" data-id-delete="'. $donnees['ID'] .'">Supprimer</li>
+			</ul>
+		</div>';
 	}
-	echo '<div id="box_' . $donnees['ID'] . '" class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
-	<p id="objectifdate-' . $donnees['ID'] .'" data-id-objectifdate="'. $donnees['ID'] .'" style="cursor:pointer">' . $donnees['objectif'] . '<br />' . $donnees['datetri'] . '</p>
-		<img id="working'. $donnees['ID'] .'" src="uploads/'. $image .'" class="bcg-dl"/>
-		<ul id="menubox-'. $donnees['ID'] .'" data-id-menubox="'. $donnees['ID'] .'" style="display:none" class="menu-objectif">
-			<li id="over-'. $donnees['ID'] .'" data-id-over="'. $donnees['ID'] .'">Terminé</li>
-				<form id="uploadimage-'. $donnees['ID'] .'" method="post" enctype="multipart/form-data">
-				  <input type="hidden" name="target_id" value="'.$donnees['ID'].'"/>
-				  <input type="file" name="target_picture">
-				  <input type="submit" id="submit-'. $donnees['ID'] .'" value="Envoyer" class="submit" />
-				</form>
-			<li id="delete-'. $donnees['ID'] .'" data-id-delete="'. $donnees['ID'] .'">Supprimer</li>
-		</ul>
-	</div>';
+	else {
+		echo '<div id="box_' . $donnees['ID'] . '" class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
+		<p id="objectifdate-' . $donnees['ID'] .'" data-id-objectifdate="'. $donnees['ID'] .'" style="cursor:pointer">' . $donnees['objectif'] . '<br />' . $donnees['datetri'] . '</p>
+			<img id="working'. $donnees['ID'] .'" src="uploads/'. $image .'" class="bcg-dl"/>
+			<ul id="menubox-'. $donnees['ID'] .'" data-id-menubox="'. $donnees['ID'] .'" style="display:none" class="menu-objectif">
+				<li id="over-'. $donnees['ID'] .'" data-id-over="'. $donnees['ID'] .'">Partager</li>
+					<form id="uploadimage-'. $donnees['ID'] .'" class="dropzone" method="post" enctype="multipart/form-data">
+					  <input type="hidden" name="target_id" value="'.$donnees['ID'].'"/>
+					  <input type="file" name="target_picture">
+					  <input type="submit" id="submit-'. $donnees['ID'] .'" value="Envoyer" class="submit" />
+					</form>
+				<li id="delete-'. $donnees['ID'] .'" data-id-delete="'. $donnees['ID'] .'">Supprimer</li>
+			</ul>
+		</div>';		
+	}
 }
